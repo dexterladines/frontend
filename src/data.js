80eda2b252f0692ff0471995b6fd77c84b14b4d6
Dexter Ladines <dexladines@gmail.com>
2022-07-18 07:41:47 +0800

    added frontend

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'C++',
+      slug: 'c++',
+      category: 'C++ Books',
+      image: '/images/cplus.jpg',
+      price: 199.99,
+      countInStock: 10,
+      rating: 4.5,
+      numReviews: 10,
+      description:
+        'API Design for C++ provides a comprehensive discussion of Application Programming Interface (API) development, from initial design through implementation, testing, documentation, release, versioning, maintenance, and deprecation.',
+    },
+    {
+      name: 'C#',
+      slug: 'csharp',
+      category: 'C# Books',
+      image: '/images/c-sharp.jpg',
+      price: 249.99,
+      countInStock: 10,
+      rating: 4,
+      numReviews: 10,
+      description:
+        "The C# Player's Guide (3rd Edition) is the ultimate guide for people starting out with C#. This version of the book is updated for C# 7.0 and Visual Studio 2017Get off the ground quickly, with a gentle introduction to C#, Visual Studio, and a step-by-step walkthrough and explanation of how to make your first C# program.",
+    },
+    {
+      name: 'Java',
+      slug: 'java',
+      category: 'Java Books',
+      image: '/images/java.jpg',
+      price: 269.99,
+      countInStock: 10,
+      rating: 3.5,
+      numReviews: 10,
+      description:
+        'The best way to learn Java is by doing. This book includes a unique project at the end of the book that requires the application of all the concepts taught previously. Working through the project will not only give you an immense sense of achievement, it’ll also help you retain the knowledge and master the language.',
+    },
+    {
+      name: 'JavaScript',
+      slug: 'javascript',
+      category: 'C++ Books',
+      image: '/images/javascript.jpg',
+      price: 249.99,
+      countInStock: 10,
+      rating: 5,
+      numReviews: 10,
+      description:
+        'This book demonstrates the capabilities of JavaScript for web application development by combining theoretical learning with code exercises and fun projects that you can challenge yourself with. The guiding principle of the book is to show how straightforward JavaScript techniques can be used to make web apps ranging from dynamic websites to simple browser-based games.',
+    },
+  ],
+};
+export default data;
